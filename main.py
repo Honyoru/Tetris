719657a6ce34b258d16c0cb4ac3637823ed0bdb4
QuentinJanuel <facemashldv@laposte.net>
2018-03-25 20:15:01 +0200

    Creation de la fenetre

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from qtido import *
-print("Hello")
-print("hehe")
+
+w = creer(500, 500)
+w.widget.setWindowTitle("Tetris")
+FPS = 10
+
+while not est_fermee(w):
+	# Du contenu incoming lol
+	attendre_pendant(w, 1000/FPS)
