bfedaef5a45e4586e25f7b764b29fe7b7a8d4633
QuentinJanuel <facemashldv@laposte.net>
2018-03-26 22:05:09 +0200

    Ca suffit les conneries ^^

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from qtido import *
 
 w = creer(500, 500)
-w.widget.setWindowTitle("Tetris the game")
-FPS = 100
+w.widget.setWindowTitle("Tetris")
+FPS = 10
 
 while not est_fermee(w):
 	# Du contenu incoming lol
